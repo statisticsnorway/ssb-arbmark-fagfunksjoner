bca831914c2c948253cc6e64769280336d62828d
Jan Sebastian Rothe <jsr@ssb.no>
2024-02-23 11:39:47 +0100

    Changed read latest to only check number after _v

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -230,6 +230,6 @@ def test_read_latest() -> None:
     result = read_latest(
         path=os.path.normpath(f"{cwd}/tests/test_data"), name="dataset"
     )
-    expected = os.path.normpath(f"{cwd}/tests/test_data/dataset3.parquet")
+    expected = os.path.normpath(f"{cwd}/tests/test_data/dataset_v3.parquet")
 
     assert result == expected, f"Expected {expected}, but got {result}."
