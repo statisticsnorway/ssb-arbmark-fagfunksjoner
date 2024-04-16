201f83b5cd2706612959a2271ef51c525a0717bc
Jan Sebastian Rothe <jsr@ssb.no>
2024-04-16 15:42:35 +0200

    Added functions for counting holidays and weekend days

--- tests/test_workdays.py
+++ tests/test_workdays.py
@@ -1,17 +1,48 @@
 import pandas as pd
+import pytest
 
+from arbmark import count_holidays
+from arbmark import count_weekenddays
 from arbmark import count_workdays
 
 
-def test_count_workdays() -> None:
-    test1_from_dates = pd.Series(
-        pd.to_datetime(["2023-01-01", "2023-12-20", "2024-05-03"])
+@pytest.fixture
+def sample_df() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "from_dates": pd.to_datetime(["2023-01-01", "2023-12-20", "2024-05-03"]),
+            "to_dates": pd.to_datetime(["2023-01-10", "2024-01-05", "2024-05-31"]),
+        }
     )
-    test1_to_dates = pd.Series(
-        pd.to_datetime(["2023-01-10", "2024-01-05", "2024-05-31"])
-    )
-    test1_result = count_workdays(test1_from_dates, test1_to_dates).to_list()
+
+
+def test_count_workdays(sample_df: pd.DataFrame) -> None:
+    test1_result = count_workdays(
+        sample_df["from_dates"], sample_df["to_dates"]
+    ).to_list()
     test1_expected = [7, 10, 18]
+
+    assert (
+        test1_result == test1_expected
+    ), f"Expected {test1_expected}, but got {test1_result}"
+
+
+def test_count_holidays(sample_df: pd.DataFrame) -> None:
+    test1_result = count_holidays(
+        sample_df["from_dates"], sample_df["to_dates"]
+    ).to_list()
+    test1_expected = [1, 3, 4]
+
+    assert (
+        test1_result == test1_expected
+    ), f"Expected {test1_expected}, but got {test1_result}"
+
+
+def test_count_weekenddays(sample_df: pd.DataFrame) -> None:
+    test1_result = count_weekenddays(
+        sample_df["from_dates"], sample_df["to_dates"]
+    ).to_list()
+    test1_expected = [3, 4, 8]
 
     assert (
         test1_result == test1_expected
