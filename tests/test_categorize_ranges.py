1b97689cfc90689e4f66c505178601584e5d85e0
Martin Skjæveland <mdd@ssb.no>
2024-10-02 11:01:14 +0200

    updated-tests

--- tests/test_categorize_ranges.py
+++ tests/test_categorize_ranges.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import pytest
 
 from arbmark import categorize_ranges
@@ -8,59 +8,50 @@ from arbmark import categorize_ranges
 def test_categorize_ranges():
     obj = pd.Series([5, 15, 25, np.nan, 35, 45, 55])
 
-    format_dict = {
-        'example_format': {
-            '10': 'Low',
-            '20': 'Medium',
-            '30': 'High'
-        }
-    }
+    format_dict = {"example_format": {"10": "Low", "20": "Medium", "30": "High"}}
 
-    expected_output = pd.Series(['Low', 'Medium', 'High', None, 'High', 'High', 'High'], dtype='object')
+    expected_output = pd.Series(
+        ["Low", "Medium", "High", None, "High", "High", "High"], dtype="object"
+    )
 
-    output = categorize_ranges(obj, 'example_format', format_dict)
+    output = categorize_ranges(obj, "example_format", format_dict)
     pd.testing.assert_series_equal(output, expected_output)
+
 
 def test_invalid_format_name():
     obj = pd.Series([5, 15, 25, 35])
 
-    format_dict = {
-        'example_format': {
-            '10': 'Low',
-            '20': 'Medium',
-            '30': 'High'
-        }
-    }
+    format_dict = {"example_format": {"10": "Low", "20": "Medium", "30": "High"}}
 
-    with pytest.raises(ValueError, match="No format with name invalid_format within given dictionary"):
-        categorize_ranges(obj, 'invalid_format', format_dict)
+    with pytest.raises(
+        ValueError, match="No format with name invalid_format within given dictionary"
+    ):
+        categorize_ranges(obj, "invalid_format", format_dict)
+
 
 def test_invalid_keys_in_format_dict():
     obj = pd.Series([5, 15, 25, 35])
 
     format_dict = {
-        'example_format': {
-            '10': 'Low',
-            'twenty': 'Medium',  # Non-digit key
-            '30': 'High'
+        "example_format": {
+            "10": "Low",
+            "twenty": "Medium",  # Non-digit key
+            "30": "High",
         }
     }
 
-    with pytest.raises(ValueError, match="Non-digits in dictionary keys. Fix your JSON-file"):
-        categorize_ranges(obj, 'example_format', format_dict)
+    with pytest.raises(
+        ValueError, match="Non-digits in dictionary keys. Fix your JSON-file"
+    ):
+        categorize_ranges(obj, "example_format", format_dict)
+
 
 def test_edge_case_values():
     obj = pd.Series([10, 20, 30, 40])
 
-    format_dict = {
-        'example_format': {
-            '10': 'Low',
-            '20': 'Medium',
-            '30': 'High'
-        }
-    }
+    format_dict = {"example_format": {"10": "Low", "20": "Medium", "30": "High"}}
 
-    expected_output = pd.Series(['Low', 'Medium', 'High', 'High'], dtype='object')
+    expected_output = pd.Series(["Low", "Medium", "High", "High"], dtype="object")
 
-    output = categorize_ranges(obj, 'example_format', format_dict)
+    output = categorize_ranges(obj, "example_format", format_dict)
     pd.testing.assert_series_equal(output, expected_output)
--- tests/test_region.py
+++ tests/test_region.py
@@ -1,4 +1,7 @@
-from arbmark import get_valid_county_codes, get_regional_special_codes, classify_mainland_not_mainland, classify_county_not_mainland
+from arbmark import classify_county_not_mainland
+from arbmark import classify_mainland_not_mainland
+from arbmark import get_regional_special_codes
+from arbmark import get_valid_county_codes
 
 
 def test_get_valid_county_codes() -> None:
@@ -12,6 +15,7 @@ def test_get_valid_county_codes() -> None:
         test1_result_first == test1_expected
     ), f"Expected {test1_expected}, but got {test1_result_first}"
 
+
 def test_get_regional_special_codes() -> None:
     year = 2021
     test2_result = sorted(get_regional_special_codes(year))
@@ -23,35 +27,44 @@ def test_get_regional_special_codes() -> None:
         test2_result_first == test2_expected
     ), f"Expected {test2_expected}, but got {test2_result_first}"
 
+
 def test_classify_mainland_not_mainland() -> None:
     municipality_numbers = pd.Series(["0101", "0301", "2100", "9999", "2211"])
-    
+
     year = 2023
-    
+
     expected_output = np.array(["Uoppgitt", "FNorge", "IFNorge", "Uoppgitt", "IFNorge"])
-    
+
     output = classify_mainland_not_mainland(municipality_numbers, year)
-    
-    assert np.array_equal(output, expected_output), f"Expected {expected_output}, but got {output}"
-    
+
+    assert np.array_equal(
+        output, expected_output
+    ), f"Expected {expected_output}, but got {output}"
+
+
 def test_classify_mainland_not_mainland() -> None:
     municipality_numbers = pd.Series(["0101", "0301", "2100", "9999", "2211"])
-    
+
     year = 2023
-    
+
     expected_output = np.array(["Uoppgitt", "FNorge", "IFNorge", "Uoppgitt", "IFNorge"])
-    
+
     output = classify_mainland_not_mainland(municipality_numbers, year)
-    
-    assert np.array_equal(output, expected_output), f"Expected {expected_output}, but got {output}"
-    
+
+    assert np.array_equal(
+        output, expected_output
+    ), f"Expected {expected_output}, but got {output}"
+
+
 def test_classify_county_not_mainland() -> None:
     municipality_numbers = pd.Series(["0101", "0301", "2100", "9999", "2211"])
-    
+
     year = 2019
-    
+
     expected_output = np.array(["01", "03", "21", "99", "22"])
-    
+
     output = classify_county_not_mainland(municipality_numbers, year, True)
-    
-    assert np.array_equal(output, expected_output), f"Expected {expected_output}, but got {output}"    
+
+    assert np.array_equal(
+        output, expected_output
+    ), f"Expected {expected_output}, but got {output}"
