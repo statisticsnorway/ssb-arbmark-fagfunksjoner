e3d73a10d10ba8cb0a95061d6391d018a4f94e57
efredborg <e.fredborg@hotmail.com>
2025-09-19 16:05:58 +0200

    starting draft

---
+++ src/arbmark/groups/koble_landbakgrunn.py
@@ -0,0 +1,76 @@
+from klass import KlassClassification
+import numpy as np
+
+def landgr_format(value):
+    if value == 'G0':
+        return 'abd'
+    elif value == 'G1':
+        return 'VES'
+    elif value == 'G2':
+        return 'IVE'
+    elif isinstance(value, (int, float)):  # Hvis verdien er et tall
+        return '999'
+    else:
+        return value  # Retur av verdien som den er hvis ingen betingelser oppfylles
+
+
+landkat = KlassClassification(546)
+landkoder = landkat.get_codes(presentation_name_pattern='{code} {name}')
+landkoder.data
+
+landkat = KlassClassification(546)
+landkoder = landkat.get_codes(presentation_name_pattern='{code} {name}')
+levels = np.sort(landkoder.data['level'].astype(int).unique())
+levels
+
+
+def koble_landbakgrunn(df: pd.DataFrame, land_col: str = None) -> pd.DataFrame:
+    if not land_col:
+        raise ValueError("You need to specify column containing country codes.")
+    landkat = KlassClassification(546)
+    landkoder = landkat.get_codes(presentation_name_pattern='{code} {name}')
+    levels = np.sort(landkoder.data['level'].unique())
+
+
+def koble_landbakgrunn_old(df):
+    landkat = KlassClassification(546)
+    landkoder = landkat.get_codes(presentation_name_pattern='{code} {name}')
+    landkoder.data
+
+    land_dict = {}
+    max_lev = landkoder.data['level'].astype('int').max()
+    for n in range(1,max_lev+1):
+        land_dict[f'land_{n}'] = landkat.get_codes(select_level=f'{n}', presentation_name_pattern='{code} {name}').to_dict()
+    land_dict
+
+    lev3 = landkoder.data.loc[landkoder.data['level'] == '3']
+    lev3_dict = lev3[['code', 'parentCode']].set_index('code').rename(columns={'parentCode' : 'land_3_to_2'}).to_dict()
+    lev3_dict['land_3_to_2']
+
+    df['land_2'] = df['la'].map(lev3_dict['land_3_to_2'])
+
+    df['land_1'] = df['land_2'].str[0:2]
+
+    df.rename(columns={'land_2': 'verdensdel', 'land_1': 'landegruppe'}, inplace=True)
+
+    vdel_mapping = {
+    'G00': 'Abc', 
+    'G11': 'UA',
+    'G12': 'UB',
+    'G13': 'UCA',
+    'G21': 'UCB',
+    'G14': 'UD',
+    'G15': 'UD',
+    'G25': 'UD',
+    'G26': 'UD',
+    'G23': '400',
+    'G22': '2',
+    'G24': '600sm'
+    }
+    df['atte_verdensregion'] = df['verdensdel'].map(vdel_mapping)
+    
+    # Opprette den nye kolonnen 'to_landgr' med de formaterte verdiene
+    df['to_landgr'] = df['landegruppe'].apply(landgr_format)
+
+    df['alle_land'] = "999"
+    return df
