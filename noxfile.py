500e6e6df22e9dd2ff6fae4a3374cf2be3a3941b
efredborg <e.fredborg@hotmail.com>
2026-06-09 15:51:35 +0200

    adding object for which python versions to test

--- noxfile.py
+++ noxfile.py
@@ -24,6 +24,7 @@ except ImportError:
 package = "arbmark"
 MAIN_PYTHON = "3.13"
 python_versions = ["3.11", "3.12", "3.13", "3.14"]
+python_versions_for_test = python_versions
 nox.needs_version = ">= 2025.2.9"
 nox.options.sessions = (
     "pre-commit",
