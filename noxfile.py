3753eff7ee954ce1019a5c77bbe182b702589eab
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2026-06-12 10:42:13 +0200

    Update to template version 2026.5.27

--- noxfile.py
+++ noxfile.py
@@ -23,8 +23,7 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "arbmark"
-MAIN_PYTHON = "3.13"
-python_versions = ["3.11", "3.12", "3.13", "3.14"]
+python_versions = ["3.13", "3.12", "3.14"]
 python_versions_for_test = python_versions
 nox.needs_version = ">= 2025.2.9"
 nox.options.sessions = (
@@ -140,7 +139,7 @@ def insert_header_in_hook(header: dict[str, str], lines: list[str]) -> str:
     return "\n".join(lines)
 
 
-@session(name="pre-commit", python=MAIN_PYTHON)
+@session(name="pre-commit", python=python_versions[0])
 def precommit(session: Session) -> None:
     """Lint using pre-commit."""
     args = session.posargs or [
@@ -187,7 +186,7 @@ def tests(session: Session) -> None:
             session.notify("coverage", posargs=[])
 
 
-@session(python=MAIN_PYTHON)
+@session(python=python_versions[0])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report", "--skip-empty"]
@@ -198,7 +197,7 @@ def coverage(session: Session) -> None:
     session.run("coverage", *args)
 
 
-@session(python=MAIN_PYTHON)
+@session(python=python_versions[0])
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
@@ -221,7 +220,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", *args)
 
 
-@session(name="docs-build", python=MAIN_PYTHON)
+@session(name="docs-build", python=python_versions[0])
 def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
@@ -238,7 +237,7 @@ def docs_build(session: Session) -> None:
     session.run("sphinx-build", *args)
 
 
-@session(python=MAIN_PYTHON)
+@session(python=python_versions[0])
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
